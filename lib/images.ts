600917be951411f4ddc0bfa8f4be2d12bc02a3ef
rattoprep <154984922+rattoprep@users.noreply.github.com>
2024-01-25 20:47:32 -0300

    Update images.ts

--- lib/images.ts
+++ lib/images.ts
@@ -1,25 +1,14 @@
-import intruder from '../public/slider/intruder.gif'
-import higi_chassis from '../public/slider/hig_chassis.gif'
-import motor_gs from '../public/slider/motorGS.gif'
-import motor_fina_gs from '../public/slider/motor_finalizacao.gif'
-import teste_fluido_freio from '../public/slider/teste_fluido_freio.gif'
-import liquido_arrefecimento from '../public/slider/liqu_arrefecimento.gif'
-import milccc from '../public/slider/1000cc.gif'
-import escape from '../public/slider/escape.gif'
-import suspencao from '../public/slider/suspencao.gif'
-import recarga_bateria from '../public/slider/recarga-bateria.gif'
-import moto_scan from '../public/slider/moto-scan.gif'
 
 export const images = [
-{src: intruder , alt: 'Shadow 750'},
-{src: higi_chassis , alt: ''},
-{src: motor_gs , alt: 'Reparos em motor BMW'},
-{src: motor_fina_gs , alt: 'Finalização'},
-{src: teste_fluido_freio , alt: 'Técnologia na revisão da sua moto'},
-{src: liquido_arrefecimento , alt: ''},
-{src: milccc , alt: 'Trabalhamos com todos os modelos'},
-{src: escape , alt: 'Troca do escapamento'},
-{src: suspencao , alt: 'Troca Óleo das Bengalas'},
-{src: recarga_bateria , alt: 'Recarga da báteria'},
-{src: moto_scan , alt: 'Moto Scan'},
+{src: "https://www.dropbox.com/scl/fi/8mev90urhkfd8j2kgrj77/intruder.gif?rlkey=wjdnf46mwvqd65vyiwwuyb9ca&dl=0" , alt: 'Shadow 750'},
+{src: "https://www.dropbox.com/scl/fi/e0cohgs4obn2d4s2fc6pv/hig_chassis.gif?rlkey=9mvh8a3q32xdudar53hg5ii50&dl=0" , alt: ''},
+{src: "https://www.dropbox.com/scl/fi/lyvdi1s00iwgmtua6lt7e/motorGS.gif?rlkey=3vokysr1ggn2pzxvx0a7wjbv4&dl=0" , alt: 'Reparos em motor BMW'},
+{src: "https://www.dropbox.com/scl/fi/u5y7d8mfu0g8c4d79g7p8/motor_finalizacao.gif?rlkey=nns4vsbwqur13c8evwzrhzweu&dl=0" , alt: 'Finalização'},
+{src: "https://www.dropbox.com/scl/fi/2ez4rsg4tn12v8p6xx49v/teste_fluido_freio.gif?rlkey=ldqljr0tbi9yi6j74ba4j4iau&dl=0" , alt: 'Técnologia na revisão da sua moto'},
+{src: "https://www.dropbox.com/scl/fi/bz16amay943x2hyvmy2xg/liqu_arrefecimento.gif?rlkey=rnu1488s8uo9g590uzulhex2j&dl=0" , alt: ''},
+{src: "https://www.dropbox.com/scl/fi/awupukhaxd3pbqjuhabin/1000cc.gif?rlkey=ryfjrz81ucoz0yz6lkezq1llc&dl=0" , alt: 'Trabalhamos com todos os modelos'},
+{src: "https://www.dropbox.com/scl/fi/o68mrgiclbmo9dyszfzuq/escape.gif?rlkey=cby6c7d2psezy5hyat9sl67ab&dl=0" , alt: 'Troca do escapamento'},
+{src: "https://www.dropbox.com/scl/fi/xgon2yi1lfjdkvr8u9xbc/suspencao.gif?rlkey=9y0zvji9kw7nfxxlpo2r5yvop&dl=0" , alt: 'Troca Óleo das Bengalas'},
+{src: "https://www.dropbox.com/scl/fi/y5tbxbn0jh6fdarifvirz/recarga-bateria.gif?rlkey=k29120jsllt9ag5nyhdxq1uji&dl=0" , alt: 'Recarga da báteria'},
+{src: "https://www.dropbox.com/scl/fi/a5rx33ts68x5g5yx1olkh/moto-scan.gif?rlkey=76ln9acccrjpwywgge57uvz4p&dl=0" , alt: 'Moto Scan'},
 ]
